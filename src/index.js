146f40168fe358547bd5c2cc2e88efe22599ec63
Yurii Popsui <yp.onward@gmail.com>
2021-03-14 19:50:55 +0200

    init: start Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  if(typeof str !== 'string'){
+    return false;
+  }
+    let result = [];
+    bracketsConfig.forEach(i => result.push(i.join('')));
+    for (let i = 0; i < str.length; i++){
+      result.forEach(i => 
+        {while(str.includes(i)){
+        str = str.replace(i, '');
+        }
+      });
+    }
+    result.forEach(i => {
+      while (str.includes(i)) {
+        str = str.replace(i, '');
+      }
+    });
+    if (result.some(elem => str.includes(elem))) {
+      i--;
+    }
+
+  return str ? false : true;
+};
+
+
+
+  
+
+
+// '()', [['(', ')']]                      // -> true
+// '((()))()', [['(', ')']]               // -> true
+// '())(', [['(', ')']]                // -> false
+
+// '([{}])', [['(', ')'], ['[', ']'], ['{', '}']]      // -> true
+
+// '[(])', [['(', ')'], ['[', ']']]             // -> false
+// '[]()', [['(', ')'], ['[', ']']]           // -> true
+// '[]()(', [['(', ')'], ['[', ']']]          // -> false
+
+// // special case: opening and closing bracket can be the same :)
+
+// '||', [['|', '|']] // -> true
+// '|()|', [['(', ')'], ['|', '|']]          // -> true
+// '|(|)', [['(', ')'], ['|', '|']]          // -> false
+// '|()|(||)||', [['(', ')'], ['|', '|']]     // -> true
